b499a838b1fa6f00671d5c9c348c7e4c0178a783
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2023-02-10 18:06:42 +0100

    template C créé

---
+++ .emacs.d/templates/00:.*.c
@@ -0,0 +1,8 @@
+/**
+ *  @filename `(file-name-nondirectory (buffer-file-name))`
+ *  @date `(format-time-string "%A, %e %B %Y.")`
+ *  @author Tristan Riehs <triehs@enseirb-matmeca.fr>
+ *  @brief ${1:description}
+ */
+
+$0
