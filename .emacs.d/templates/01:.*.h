6e3015cb13e9dc2691b42e77dbeb349878b4d7e5
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2023-03-03 12:51:49 +0100

    premiers templates ajoutés

---
+++ .emacs.d/templates/01:.*.h
@@ -0,0 +1,13 @@
+/**
+ *  @filename `(file-name-nondirectory (buffer-file-name))`
+ *  @date `(format-time-string "%A, %e %B %Y.")`
+ *  @author Tristan Riehs <triehs@enseirb-matmeca.fr>
+ *  @brief ${1:description}
+ */
+
+#ifndef ${1:_`(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`_H_}
+#define $1
+
+$0
+
+#endif /* $1 */
